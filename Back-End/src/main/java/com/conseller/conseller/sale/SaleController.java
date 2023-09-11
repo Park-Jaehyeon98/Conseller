4fd6a2439355a6ecd5cc63b70a6f7252ef791efe
박대화 <zlzl_321@naver.com>
2023-09-11 17:55:21 +0900

    Feat : 판매 등록 기능 230911 1755

--- Back-End/src/main/java/com/conseller/conseller/sale/SaleController.java
+++ Back-End/src/main/java/com/conseller/conseller/sale/SaleController.java
@@ -1,14 +1,39 @@
 package com.conseller.conseller.sale;
 
+import com.conseller.conseller.entity.Sale;
+import com.conseller.conseller.sale.dto.request.RegistSaleRequest;
+import com.conseller.conseller.sale.dto.request.SaleListRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/sale")
 public class SaleController {
+    private final SaleService saleService;
+
+    private final String SUCCESS = "success";
+    @PostMapping
+    public ResponseEntity<List<Sale>> getSaleList(@RequestBody SaleListRequest request) {
+
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @PostMapping("/regist")
+    public ResponseEntity<String> registSale(@RequestBody RegistSaleRequest request) {
+        saleService.registSale(request);
+
+        return new ResponseEntity<>(SUCCESS, HttpStatus.OK);
+    }
+
 
 }
--- Back-End/src/main/java/com/conseller/conseller/sale/SaleService.java
+++ Back-End/src/main/java/com/conseller/conseller/sale/SaleService.java
@@ -1,12 +1,58 @@
 package com.conseller.conseller.sale;
 
+import com.conseller.conseller.entity.Gifticon;
+import com.conseller.conseller.entity.Sale;
+import com.conseller.conseller.entity.User;
+import com.conseller.conseller.sale.dto.request.RegistSaleRequest;
+import com.conseller.conseller.sale.dto.response.DetailSaleResponse;
+import com.conseller.conseller.sale.enums.SaleStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SaleService {
     private final SaleRepository saleRepository;
+    private final UserRepository userRepository;
+    private final GifticonRepository gifticonRepository;
 
+    public List<Sale> getSaleList() {
+
+        return null;
+    }
+
+    public void registSale(RegistSaleRequest request) {
+
+        User user = userRepository.findById(request.getUserIdx()).orElseThrow();
+        Gifticon gifticon = gifticonRepository.findById(request.getGifticonIdx()).orElseThrow();
+
+        Sale sale = Sale.builder()
+                .user(user)
+                .salePrice(request.getSalePrice())
+                .saleText(request.getSaleText())
+                .gifticon(gifticon)
+                .saleStatus(SaleStatus.IN_PROGRESS)
+                .build();
+
+        saleRepository.save(sale);
+    }
+
+    public DetailSaleResponse detailSale(Long saleIdx) {
+        Sale sale = saleRepository.findById(saleIdx).orElseThrow();
+
+        User user = userRepository.findById(sale.getUser().getUserIdx()).orElseThrow();
+
+        DetailSaleResponse response = DetailSaleResponse.builder()
+                .saleUserIdx(user.getUserIdx())
+                .saleText(sale.getSaleText())
+                .saleEndedDate(sale.getSaleEndedDate())
+                .saleCreatedDate(sale.getSaleCreatedDate())
+                .saleUserNickname(user.getUserNickname())
+                .build();
+
+        return null;
+    }
 
 }

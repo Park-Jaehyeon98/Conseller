d64b17c04a1a8dbd533852853fb63446f51ab9c2
박대화 <zlzl_321@naver.com>
2023-09-11 17:55:32 +0900

    Feat : 판매 등록 기능 230911 1755

---
+++ Back-End/src/main/java/com/conseller/conseller/sale/dto/request/RegistSaleRequest.java
@@ -0,0 +1,19 @@
+package com.conseller.conseller.sale.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class RegistSaleRequest {
+    private Integer salePrice;
+
+    private String saleText;
+
+    private Long gifticonIdx;
+
+    private Long userIdx;
+
+}

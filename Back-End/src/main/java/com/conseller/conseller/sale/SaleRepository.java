1db6c7962404bbb9e56332f20daa1acd1eef2122
박대화 <zlzl_321@naver.com>
2023-09-11 10:46:43 +0900

    Feat : 판매 엔티티 변경 230911 1046

--- Back-End/src/main/java/com/conseller/conseller/sale/SaleRepository.java
+++ Back-End/src/main/java/com/conseller/conseller/sale/SaleRepository.java
@@ -2,9 +2,11 @@ package com.conseller.conseller.sale;
 
 import com.conseller.conseller.entity.Sale;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
 }
--- Back-End/src/main/java/com/conseller/conseller/sale/SaleService.java
+++ Back-End/src/main/java/com/conseller/conseller/sale/SaleService.java
@@ -1,4 +1,12 @@
 package com.conseller.conseller.sale;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class SaleService {
+    private final SaleRepository saleRepository;
+
+
 }

2fe8075fe523e481cf53fa2e5b73925998272409
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-09-13 17:51:04 +0900

    Feat: 회원가입 DTO 유효성 검증 추가 20230913 1751

--- Back-End/src/main/java/com/conseller/conseller/user/dto/SignUpDto.java
+++ Back-End/src/main/java/com/conseller/conseller/user/dto/SignUpDto.java
@@ -1,16 +1,40 @@
 package com.conseller.conseller.user.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import com.sun.istack.NotNull;
+import lombok.*;
 
-@Getter @Setter
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+@ToString
+@Getter @Setter @Builder
+@AllArgsConstructor
 public class SignUpDto {
 
+    @NotBlank
+    @Size(min = 8)
     private String userId;
+
+    @NotBlank
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$")
     private String userPassword;
+
+    @NotBlank
+    @Email
     private String userEmail;
+
+    @NotBlank
     private String userPhoneNumber;
+
+    @NotBlank
+    @Size(min = 8)
     private String userNickname;
+
+    @NotBlank
     private String userAccount;
+
+    @NotBlank
     private String userAccountBank;
 }

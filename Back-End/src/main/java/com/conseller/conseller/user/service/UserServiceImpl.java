7945d6aa0ad831040b7435da1567d51ff680313d
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-09-14 16:36:05 +0900

    Feat: 회원 등록 메소드 수정 및 체크 메서드 구현 20230914 1636

--- Back-End/src/main/java/com/conseller/conseller/user/service/UserServiceImpl.java
+++ Back-End/src/main/java/com/conseller/conseller/user/service/UserServiceImpl.java
@@ -2,6 +2,8 @@ package com.conseller.conseller.user.service;
 
 import com.conseller.conseller.entity.User;
 import com.conseller.conseller.user.UserRepository;
+import com.conseller.conseller.user.UserValidator;
+import com.conseller.conseller.user.dto.InfoValidationDto;
 import com.conseller.conseller.user.dto.SignUpDto;
 import com.conseller.conseller.user.enums.AccountBanks;
 import com.conseller.conseller.user.enums.UserStatus;
@@ -10,17 +12,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final UserValidator userValidator;
 
     @Override
     public User register(SignUpDto signUpDto) {
+        userValidator.signUpDtoValidate(signUpDto);
+
         User user = User.builder()
                 .userId(signUpDto.getUserId())
                 .userPassword(signUpDto.getUserPassword())
@@ -28,6 +33,9 @@ public class UserServiceImpl implements UserService {
                 .userDeposit(0)
                 .userNickname(signUpDto.getUserNickname())
                 .userPhoneNumber(signUpDto.getUserPhoneNumber())
+                .userGender(signUpDto.getUserGender())
+                .userAge(signUpDto.getUserAge())
+                .userName(signUpDto.getUserName())
                 .userAccount(signUpDto.getUserAccount())
                 .userRestrictCount(0)
                 .userStatus(UserStatus.ACTIVE)
@@ -37,4 +45,46 @@ public class UserServiceImpl implements UserService {
         userRepository.save(user);
         return user;
     }
+
+    @Override
+    public InfoValidationDto checkNickname(String nickname) {
+        boolean nicknameExists = userRepository.existsByUserNickname(nickname);
+
+        return InfoValidationDto.builder()
+                .status(nicknameExists ? 0 : 1)
+                .message(nicknameExists ? "이미 존재하는 닉네임 입니다." : "사용할 수 있는 닉네임 입니다")
+                .build();
+    }
+
+    @Override
+    public InfoValidationDto checkId(String id) {
+        boolean idExists = userRepository.existsByUserId(id);
+
+        return InfoValidationDto.builder()
+                .status(idExists ? 0 : 1)
+                .message(idExists ? "이미 존재하는 아이디 입니다." : "사용할 수 있는 아이디 입니다")
+                .build();
+    }
+
+    @Override
+    public InfoValidationDto checkEmail(String email) {
+        boolean emailExists = userRepository.existsByUserEmail(email);
+
+        return InfoValidationDto.builder()
+                .status(emailExists ? 0 : 1)
+                .message(emailExists ? "이미 존재하는 이메일 입니다." : "사용할 수 있는 이메일 입니다")
+                .build();
+    }
+
+    @Override
+    public InfoValidationDto checkPhoneNumber(String phoneNumber) {
+        boolean phoneNumberExists = userRepository.existsByUserPhoneNumber(phoneNumber);
+
+        return InfoValidationDto.builder()
+                .status(phoneNumberExists ? 0 : 1)
+                .message(phoneNumberExists ? "이미 존재하는 전화번호 입니다." : "사용할 수 있는 전화번호 입니다")
+                .build();
+    }
+
+
 }

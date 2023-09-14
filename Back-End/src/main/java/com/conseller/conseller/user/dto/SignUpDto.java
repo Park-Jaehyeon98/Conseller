5d6c37b8656d88eb54be106977099d9699dfa0fc
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-09-14 16:35:13 +0900

    Refactor: SignUpDto 성별, 이름, 나이 필드 추가 및 어노테이션 수정 20230914 1635

--- Back-End/src/main/java/com/conseller/conseller/user/dto/SignUpDto.java
+++ Back-End/src/main/java/com/conseller/conseller/user/dto/SignUpDto.java
@@ -3,38 +3,43 @@ package com.conseller.conseller.user.dto;
 import com.sun.istack.NotNull;
 import lombok.*;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 
 @ToString
 @Getter @Setter @Builder
 @AllArgsConstructor
 public class SignUpDto {
 
-    @NotBlank
-    @Size(min = 8)
+    @NotBlank(message = "아이디를 입력해야 합니다.")
     private String userId;
 
-    @NotBlank
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$")
+    @NotBlank(message = "비밀번호를 입력해야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$", message = "비밀번호는 영문, 숫자, 특수기호 포함 8자리 이상이여야 합니다.")
     private String userPassword;
 
-    @NotBlank
-    @Email
+    @NotBlank(message = "이메일을 입력해야 합니다.")
+    @Email(message = "이메일 형식에 맞지 않습니다.")
     private String userEmail;
 
-    @NotBlank
+    @NotBlank(message = "핸드폰 번호를 입력해야 합니다.")
     private String userPhoneNumber;
 
-    @NotBlank
-    @Size(min = 8)
+    @NotBlank(message = "별명을 입력해야 합니다.")
     private String userNickname;
 
-    @NotBlank
+    @NotNull
+    private Character userGender;
+
+    @Positive(message = "나이는 정수로 입력이 가능합니다.")
+    @Min(1)
+    private Integer userAge;
+
+    @NotBlank(message = "이름을 입력해야 합니다.")
+    private String userName;
+
+    @NotBlank(message = "계좌번호를 입력해야 합니다.")
     private String userAccount;
 
-    @NotBlank
+    @NotBlank(message = "거래은행을 선택해야 합니다.")
     private String userAccountBank;
 }

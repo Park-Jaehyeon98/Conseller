228cbcb40a41c7ee199d19d5d680a25ff92242db
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-09-18 17:34:18 +0900

    Feat: User Service layer 로그인 기능 추가 20230918 1734

--- Back-End/src/main/java/com/conseller/conseller/user/service/UserService.java
+++ Back-End/src/main/java/com/conseller/conseller/user/service/UserService.java
@@ -1,8 +1,10 @@
 package com.conseller.conseller.user.service;
 
 import com.conseller.conseller.entity.User;
+import com.conseller.conseller.user.dto.request.LoginRequest;
 import com.conseller.conseller.user.dto.response.InfoValidationRequest;
 import com.conseller.conseller.user.dto.request.SignUpRequest;
+import com.conseller.conseller.user.dto.response.LoginResponse;
 
 public interface UserService {
 
@@ -11,4 +13,6 @@ public interface UserService {
     public InfoValidationRequest checkId(String id);
     public InfoValidationRequest checkEmail(String email);
     public InfoValidationRequest checkPhoneNumber(String phoneNumber);
+
+    public LoginResponse login(LoginRequest loginRequest);
 }
--- Back-End/src/main/java/com/conseller/conseller/user/service/UserServiceImpl.java
+++ Back-End/src/main/java/com/conseller/conseller/user/service/UserServiceImpl.java
@@ -3,15 +3,23 @@ package com.conseller.conseller.user.service;
 import com.conseller.conseller.entity.User;
 import com.conseller.conseller.user.UserRepository;
 import com.conseller.conseller.user.UserValidator;
+import com.conseller.conseller.user.dto.request.LoginRequest;
 import com.conseller.conseller.user.dto.response.InfoValidationRequest;
 import com.conseller.conseller.user.dto.request.SignUpRequest;
+import com.conseller.conseller.user.dto.response.LoginResponse;
 import com.conseller.conseller.user.enums.AccountBanks;
 import com.conseller.conseller.user.enums.UserStatus;
+import com.conseller.conseller.utils.JwtToken;
+import com.conseller.conseller.utils.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -21,6 +29,8 @@ public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
     private final UserValidator userValidator;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public User register(SignUpRequest signUpRequest) {
@@ -43,6 +53,38 @@ public class UserServiceImpl implements UserService {
                 .build();
 
         return userRepository.save(user);
+    }
+
+    @Override
+    public LoginResponse login(LoginRequest loginRequest) {
+
+        // 1. username + password 를 기반으로 Authentication 객체 생성
+        // 이때 authentication 은 인증 여부를 확인하는 authenticated 값이 false
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getUserPassword());
+
+        // 2. 실제 검증. authenticate() 메서드를 통해 요청된 User에 대한 검증 진행
+        // authenticate 메서드가 실행될 때 CustomUserDetailsService 에서 만든 loadUserByUsername 메서드 실행
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+
+        // 3. 인증 정보를 기반으로 JWT 토큰 생성
+        JwtToken jwtToken = jwtTokenProvider.createToken(authentication);
+
+        // 유저 정보를 따로 가져오기 위한 호출
+        Optional<User> loginedUser = userRepository.findByUserId(loginRequest.getUserId());
+        User user = loginedUser.get();
+
+        if (jwtToken.getAccessToken() != null) {
+            loginedUser = userRepository.findByUserId(loginRequest.getUserId());
+        }
+
+        // 4. 토큰 정보로 response 생성 후 리턴
+        return LoginResponse.builder()
+                .userIdx(user.getUserIdx())
+                .userNickname(user.getUserNickname())
+                .accessToken(jwtToken.getAccessToken())
+                .refreshToken(jwtToken.getRefreshToken())
+                .build();
     }
 
     @Override

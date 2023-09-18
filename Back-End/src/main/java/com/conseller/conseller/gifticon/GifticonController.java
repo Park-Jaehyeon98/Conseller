2b8ac7b3b668d25a0cb1521e205de8c95303c0ac
ki_mensoo <ekclstkfka5@gmail.com>
2023-09-19 01:34:04 +0900

    Feat: 기프티콘 도메인 구조 잡기 20230919 0134

---
+++ Back-End/src/main/java/com/conseller/conseller/gifticon/GifticonController.java
@@ -0,0 +1,4 @@
+package com.conseller.conseller.gifticon;
+
+public class GifticonController {
+}
---
+++ Back-End/src/main/java/com/conseller/conseller/gifticon/service/GifticonService.java
@@ -0,0 +1,4 @@
+package com.conseller.conseller.gifticon.service;
+
+public interface GifticonService {
+}

759ff5fa06ec6b20a17ea9f7d9fb07d5324d1fe2
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-09-19 10:18:49 +0900

    Feat: 에러 핸들링 기능 추가 20230919 1018

---
+++ Back-End/src/main/java/com/conseller/conseller/exception/GlobalExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.conseller.conseller.exception;
+
+import com.conseller.conseller.utils.CommonResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final String COMMON_CODE = "0001";
+    private static final String IllEGAL_CODE = "0002";
+    private static final String USER_NOT_FOUND_CODE = "0003";
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<CommonResponse> handleIllegalException(RuntimeException exception) {
+        CommonResponse commonResponse = CommonResponse.builder()
+                .code(COMMON_CODE)
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.badRequest()
+                .body(commonResponse);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<CommonResponse> handleRuntimeException(RuntimeException exception) {
+        CommonResponse commonResponse = CommonResponse.builder()
+                .code(IllEGAL_CODE)
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.badRequest()
+                .body(commonResponse);
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<CommonResponse> handleUserNameNotFoundException(RuntimeException exception) {
+        CommonResponse commonResponse = CommonResponse.builder()
+                .code(USER_NOT_FOUND_CODE)
+                .message(exception.getMessage())
+                .build();
+
+        return ResponseEntity.badRequest()
+                .body(commonResponse);
+    }
+}

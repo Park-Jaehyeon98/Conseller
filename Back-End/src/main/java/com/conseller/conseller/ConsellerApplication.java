e3a6fc623912f21f3a8d788f2908e7123b16afd1
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-09-22 09:30:06 +0900

    Refactor: S3를 위한 static 필드 추가 20230922 0930

--- Back-End/src/main/java/com/conseller/conseller/ConsellerApplication.java
+++ Back-End/src/main/java/com/conseller/conseller/ConsellerApplication.java
@@ -7,6 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 @EnableJpaAuditing
 public class ConsellerApplication {
+    //S3 위한 처리
+    static {
+        System.setProperty("com.amazonaws.sdk.disableEc2Metadata", "true");
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ConsellerApplication.class, args);

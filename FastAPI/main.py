16e2f25f9bd97030e41666c03132a4d60941d0b6
박재현 <holizon9@naver.com>
2023-09-24 17:13:25 +0900

    Feat : pytesseract OCR로 기프티콘 crop 및 유효 데이터 처리 230924 1713

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,84 +1,110 @@
-# FastAPI CRUD 연습
-
 from fastapi import FastAPI, HTTPException, UploadFile
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from typing import List
-import sys
 import pytesseract
-import os
+from PIL import Image
+import io
+
 app = FastAPI()
 
-UPLOAD_DIR = "images/"
-
-# 메모를 저장하기 위한 데이터베이스 대체
-database = []
-
-class Memo(BaseModel):
-    id: int
-    content: str
-
-# 이미지 전송
-@app.post("/gifticon/")
-def create_image(image: UploadFile):
+def crop_img(image_file):
     try:
-        file_path = f"{UPLOAD_DIR}{image.filename}"
+        image = Image.open(image_file)
 
-      
+        width, height = image.size
+        top = 0
+        top_bottom = int(height * 0.44)
+        bottom = height
 
-        with open(file_path, "wb") as image_file:
-                image_file.write(image.file.read())
+        left = 0
+        right = width
 
-        print("message: 이미지 업로드가 완료되었습니다.")
-        config = ('-l kor+eng --oem 3 --psm 11')
+        # 이미지 자르기
+        top_cropped_image = image.crop((left, top, right, top_bottom))
+        bottom_cropped_image = image.crop((left, top_bottom, right, bottom))
 
-        # print(pytesseract.image_to_string(file_path, lang='eng'))
-        print(pytesseract.image_to_string(file_path, config=config))
+        # 새로운 파일로 저장
+        top_cropped_image.save("./top_crop/cropped_image.jpg")
+        bottom_cropped_image.save("./bottom_crop/cropped_image.jpg")
 
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            print(f"message '{image.filename}' 삭제 완료.")
-        else:
-            print(f"error: '{image.filename}'을 찾을 수 없습니다.")
-        
-        return JSONResponse(content={"message": "이미지 업로드 후 삭제가 완료되었습니다."}, status_code=200)
+        ocr_result = perform_ocr()
+                                 
+        # 성공적으로 저장되면 경로 반환
+        return ocr_result
+
     except Exception as e:
-        return JSONResponse(content={"error": str(e)}, status_code=500)
+        raise HTTPException(status_code=500, detail=str(e))
+    
+def crop_giftishow(image_file):
+    try:
+        image = Image.open(image_file)
 
+        width, height = image.size
+        top = 0
+        top_bottom = int(height * 0.5)
+        bottom = height
 
-# 메모 생성
-@app.post("/memos/", response_model=Memo)
-def create_memo(memo: Memo):
-    database.append(memo)
-    return memo
+        left = 0
+        right = width
 
-# 모든 메모 조회
-@app.get("/memos/", response_model=List[Memo])
-def read_memos(skip: int = 0, limit: int = 10):
-    return database[skip : skip + limit]
+        # 이미지 자르기
+        top_cropped_image = image.crop((left, top, right, top_bottom))
+        bottom_cropped_image = image.crop((left, top_bottom, right, bottom))
 
-# 특정 메모 조회
-@app.get("/memos/{memo_id}", response_model=Memo)
-def read_memo(memo_id: int):
-    memo = next((m for m in database if m.id == memo_id), None)
-    if memo is None:
-        raise HTTPException(status_code=404, detail="Memo not found")
-    return memo
+        # 새로운 파일로 저장
+        top_cropped_image.save("./top_crop/cropped_image.jpg")
+        bottom_cropped_image.save("./bottom_crop/cropped_image.jpg")
 
-# 메모 업데이트
-@app.put("/memos/{memo_id}", response_model=Memo)
-def update_memo(memo_id: int, updated_memo: Memo):
-    existing_memo = next((m for m in database if m.id == memo_id), None)
-    if existing_memo is None:
-        raise HTTPException(status_code=404, detail="Memo not found")
-    existing_memo.content = updated_memo.content
-    return existing_memo
+        ocr_result = perform_ocr()
 
-# 메모 삭제
-@app.delete("/memos/{memo_id}", response_model=Memo)
-def delete_memo(memo_id: int):
-    existing_memo = next((m for m in database if m.id == memo_id), None)
-    if existing_memo is None:
-        raise HTTPException(status_code=404, detail="Memo not found")
-    database.remove(existing_memo)
-    return existing_memo
+        return ocr_result
+                                 
+        # 성공적으로 저장되면 경로 반환
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# OCR 함수 정의
+def perform_ocr():
+    try:
+        # 이미지를 PIL Image로 열기
+        image = Image.open("./bottom_crop/cropped_image.jpg")
+        
+        # OCR 수행
+        config = ('-l kor+eng --oem 3 --psm 11')
+        ocr_result = pytesseract.image_to_string(image, config=config)
+        ocr_result = ocr_result.replace("\n", " ")
+
+        result = ocr_result.split()
+        
+        
+        for i in range(0, len(result)):
+            if len(result[i]) == 4 and (result[i] == "0525" or result[i] == "Gs25" or result[i] == "6525"):
+                print(result[i])
+                result[i] = "GS25"
+            elif len(result[i]) == 9 and result[i] == "0525[금액권]":
+                result[i] = "GS25[금액권]"
+            elif len(result[i]) == 1 and (result[i] == "「" or result[i] == "7" or result[i] == "1"):
+                result[i] = "T"
+            elif len(result[i]) == 3 and result[i] == "타벅스":
+                result[i] = "스타벅스"
+
+        return result
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+# 이미지 업로드 및 OCR 수행
+@app.post("/gifticon")
+def ocr_image(image: UploadFile):
+    try:
+        result = ""
+        result = crop_img(image.file)
+        for i in range(len(result)):
+            if len(result[i]) == 4 and result[i] == "기프티쇼":
+                result = crop_giftishow(image.file)
+                break
+        
+        return JSONResponse(content={"message": "OCR 결과", "text": result}, status_code=200)
+    except HTTPException as e:
+        return JSONResponse(content={"error": e.detail}, status_code=e.status_code)

103509fcd271e7585af68bae8a8a8bb3f0d379f7
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-09-25 02:28:44 +0900

    Refactor: Security 설정 수정 20230925 0228

--- Back-End/src/main/java/com/conseller/conseller/config/SecurityConfig.java
+++ Back-End/src/main/java/com/conseller/conseller/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import com.conseller.conseller.utils.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -29,7 +30,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/user").permitAll()
+                .antMatchers(HttpMethod.POST, "/user").permitAll()
                 .antMatchers("/user/login").permitAll()
                 .antMatchers("/user/encode/**").permitAll()
                 .antMatchers("/user/id").permitAll()

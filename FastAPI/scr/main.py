b4468c51caafca05bdcfb112975f751bc4d04797
박재현 <holizon9@naver.com>
2023-09-25 23:34:05 +0900

    fix : jenkinsfile, dockerfile 수정 230925 2321

--- FastAPI/scr/main.py
+++ FastAPI/scr/main.py
@@ -279,6 +279,3 @@ def ocr_image(category: int = Form(...), image: UploadFile = File(...)):
 
     except HTTPException as e:
         raise HTTPException(status_code=e.status_code, detail=e.detail)
-    
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=False)

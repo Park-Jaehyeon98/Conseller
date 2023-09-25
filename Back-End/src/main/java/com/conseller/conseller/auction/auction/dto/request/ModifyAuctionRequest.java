aefcdf1c3f2e586d1d5e20f9171ef402690dc68c
박대화 <zlzl_321@naver.com>
2023-09-25 23:28:44 +0900

    Feat : 컨버터 string -> Localdatetime 추가 230925 2328

--- Back-End/src/main/java/com/conseller/conseller/auction/auction/dto/request/ModifyAuctionRequest.java
+++ Back-End/src/main/java/com/conseller/conseller/auction/auction/dto/request/ModifyAuctionRequest.java
@@ -5,13 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ModifyAuctionRequest {
-    private LocalDateTime auctionEndDate;
+    private String auctionEndDate;
     private String auctionText;
 }

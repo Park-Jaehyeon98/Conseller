a81330da7391cdc764ced5f7199bf63940c58de9
박대화 <zlzl_321@naver.com>
2023-09-25 22:03:56 +0900

    Feat : 컨버터 싱글턴 수정 230925 1757

--- Back-End/src/main/java/com/conseller/conseller/auction/auction/dto/response/AuctionItemData.java
+++ Back-End/src/main/java/com/conseller/conseller/auction/auction/dto/response/AuctionItemData.java
@@ -1,12 +1,10 @@
 package com.conseller.conseller.auction.auction.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuctionItemData {
--- Back-End/src/main/java/com/conseller/conseller/auction/auction/dto/response/AuctionListResponse.java
+++ Back-End/src/main/java/com/conseller/conseller/auction/auction/dto/response/AuctionListResponse.java
@@ -1,14 +1,12 @@
 package com.conseller.conseller.auction.auction.dto.response;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.List;
 
 @Setter
 @Getter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 public class AuctionListResponse {

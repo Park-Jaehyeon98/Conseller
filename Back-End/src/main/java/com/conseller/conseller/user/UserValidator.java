373e77c7c2a88c8acb6fcfb3e69a1e04a0f10619
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-10-01 12:34:57 +0900

    Refactor: 로그인 유저의 정보가 유효한지 검증하는 로직 분리 20231001 1234
    기존의 로그인 로직에서 검증까지 하던 부분을 UserValidator가 담당하도록 분리함.

--- Back-End/src/main/java/com/conseller/conseller/user/UserValidator.java
+++ Back-End/src/main/java/com/conseller/conseller/user/UserValidator.java
@@ -1,8 +1,14 @@
 package com.conseller.conseller.user;
 
+import com.conseller.conseller.entity.User;
+import com.conseller.conseller.exception.CustomException;
+import com.conseller.conseller.exception.CustomExceptionStatus;
+import com.conseller.conseller.user.dto.request.LoginRequest;
 import com.conseller.conseller.user.dto.request.SignUpRequest;
+import com.conseller.conseller.user.enums.UserStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -36,5 +42,26 @@ public class UserValidator {
 
     public boolean isVaildByUserIdx(long userIdx) {
         return userRepository.existsByUserIdx(userIdx);
+    }
+
+    public void validateLogin(LoginRequest request) {
+        // 입력 id 정보가 유효한지 확인
+        User user = userRepository.findByUserId(request.getUserId())
+                .orElseThrow(() -> new CustomException(CustomExceptionStatus.WRONG_ID));
+
+        // 입력한 password 정보가 유효한지 확인
+        if (!user.checkPassword(new BCryptPasswordEncoder(), request.getUserPassword())) {
+            throw new CustomException(CustomExceptionStatus.WRONG_PW);
+        }
+
+        // 입력한 유저가 탈퇴한 유저인지 확인
+        if (user.getUserDeletedDate() != null) {
+            throw new CustomException(CustomExceptionStatus.RESTRICT);
+        }
+
+        //입력한 유저가 사용 제한된 유저인지 확인
+        if (UserStatus.RESTRICTED.getStatus().equals(user.getUserStatus())) {
+            throw new CustomException(CustomExceptionStatus.RESTRICT);
+        }
     }
 }

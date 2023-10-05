48a48cad35478592120d78785cc29dd0de0ca5e1
DESKTOP-BLHP263\SSAFY <ekclstkfka5@gmail.com>
2023-10-05 10:19:45 +0900

    Refactor: 기프티콘 삭제 로직 변경 20231005 1005
    기프티콘 삭제 로직을 변경함.

--- Back-End/src/main/java/com/conseller/conseller/gifticon/service/GifticonService.java
+++ Back-End/src/main/java/com/conseller/conseller/gifticon/service/GifticonService.java
@@ -10,7 +10,7 @@ public interface GifticonService {
     public void registGifticon(long userIdx, GifticonRegisterRequest gifticonRegisterRequest, String allImageUrl, String dataImageUrl);
 
     //기프티콘을 삭제하고 이미지 url을 넘긴다.
-    public ImageUrlsResponse deleteGifticon(long gifticonIdx);
+    public void deleteGifticon(long gifticonIdx);
 
     //정각에 기프티콘 유효기간 알람을 보내주는 기능
     public void checkGifticonEndDate();

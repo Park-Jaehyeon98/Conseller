77676997581f39ca1051c16997a20cdfd507b946
박대화 <zlzl_321@naver.com>
2023-10-05 10:25:59 +0900

    Fix : 알림 날짜 포맷 변경 231005 1025

--- Back-End/src/main/java/com/conseller/conseller/notification/dto/mapper/NotificationMapper.java
+++ Back-End/src/main/java/com/conseller/conseller/notification/dto/mapper/NotificationMapper.java
@@ -2,9 +2,9 @@ package com.conseller.conseller.notification.dto.mapper;
 
 import com.conseller.conseller.entity.NotificationEntity;
 import com.conseller.conseller.notification.dto.response.NotificationItemData;
+import com.conseller.conseller.utils.DateTimeConverter;
 import org.mapstruct.IterableMapping;
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
 import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
 
@@ -16,8 +16,16 @@ public interface NotificationMapper {
 
     //NotificationList -> NotificationItemDataList 매핑
     @Named("N2N")
-    @Mapping(source = "notification.notificationContent", target = "notificationStatus")
-    NotificationItemData notificationToItemData(NotificationEntity notification);
+    default NotificationItemData notificationToItemData(NotificationEntity notification) {
+        NotificationItemData itemData = new NotificationItemData();
+
+        itemData.setNotificationIdx(notification.getNotificationIdx());
+        itemData.setNotificationType(notification.getNotificationType());
+        itemData.setNotificationCreatedDate(DateTimeConverter.getInstance().convertString(notification.getNotificationCreatedDate()));
+        itemData.setNotificationStatus(notification.getNotificationContent());
+
+        return itemData;
+    }
 
     @IterableMapping(qualifiedByName = "N2N")
     List<NotificationItemData> notificationsToItemDatas(List<NotificationEntity> notificationEntityList);
